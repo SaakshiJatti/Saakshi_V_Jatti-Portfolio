5660c0378ea0219398f70c0fcdfe618b235b5cad
SaakshiJatti <saakshivjatti@gmail.com>
2025-08-09 22:12:50 +0530

    Correct Vite config for build

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,25 +5,24 @@ import tagger from "@dhiwise/component-tagger";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Correctly define __dirname for ES modules
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   resolve: {
     alias: {
-      "three/examples/js/libs/stats.min": path.resolve(__dirname, "empty-module.js")
-    }
+      "three/examples/js/libs/stats.min": path.resolve(__dirname, "empty-module.js"),
+    },
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-  }
+  },
 });

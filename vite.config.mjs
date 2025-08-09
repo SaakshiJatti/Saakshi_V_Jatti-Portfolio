99f541cee02c3e5fa60c446e36673a653311fb04
SaakshiJatti <saakshivjatti@gmail.com>
2025-08-09 22:42:39 +0530

    Feat: Restore final Vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Correctly define __dirname for ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
-// A minimal config for debugging the Vercel build
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
   },
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "three/examples/js/libs/stats.min": path.resolve(__dirname, "empty-module.js"),
+    },
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+  },
+});
